e174253e32478805a2cbf648888f811b8e1d9326
MacInnes <califormula@gmail.com>
2018-10-02 15:29:22 -0600

    removed relationships from merchant serializer

--- app/serializers/merchant_serializer.rb
+++ app/serializers/merchant_serializer.rb
@@ -1,9 +1,3 @@
 class MerchantSerializer < ActiveModel::Serializer
   attributes :id, :name
-
-  has_many :items
-  has_many :invoices
-  has_many :customers
-  has_many :transactions
-  has_many :invoice_items
 end
